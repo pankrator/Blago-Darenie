f0be0e144a53f881f9a664651def3fd509ab8e6d
Nikolay Stoykov <nikolaisstoykov@gmail.com>
2014-04-26 11:19:06 +0300

    Charity plugin added

---
+++ wp-content/plugins/charity/charity-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Charity Plugin
+ * Plugin URI: fsdfdsfds
+ * Description: Charity plugin that addds functionality for creating charity campaigns and exchanging items and good for charity
+ * Version: 1.0
+ * Author: Sofia University - Students in second grade Computer Science and Software Engineering
+ * Author URI: gsfsdfdsfs
+ * License: "MIT"
+ */
